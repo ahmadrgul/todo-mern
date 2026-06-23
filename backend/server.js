8eac4fe9f4b14e715696e02dbf7dd2e38a6d3969
ahmadrgul <ahmadrg91@gmail.com>
2026-06-23 16:28:02 +0500

    feat: add cors middleware

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ require("dotenv").config()
 
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
+
 const taskRoutes = require("./routes/tasks")
 
 const app = express()
 
+app.use(cors({ origin: process.env.FRONTEND_URL }))
 app.use(express.json())
 app.use("/api/tasks", taskRoutes)
 
