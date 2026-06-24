5fbb0be906995ae42864be8479e65ff3afaccd5f
ahmadrgul <ahmadrg91@gmail.com>
2026-06-24 11:54:36 +0500

    update: update api functions for better error handling

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -7,40 +7,21 @@ const axiosInstance = axios.create({
 export const getTasks = async () => {
     try {
         const res = await axiosInstance.get("/tasks");
-        return res.data
+        return res
     } catch (err) {
         if (err instanceof AxiosError) {
             if (!err.response) {
-                console.log("Network Error")
+                throw Error("Network Error!")
             } else {
-                console.log(err.response.status, ":", err.response.data.error)
+                // throw Error("Sorry! Something went wrong!")
             }
-        }
-        else {
-            console.log("Another Error")
+        } else {
+            throw Error("Sorry, Something went wrong!")
         }
     }
 }
 
-export const getTask = async (id) => {
-    try {
-        const res = await axiosInstance.get(`/tasks/${id}`);
-        return res.data
-    } catch (err) {
-        if (err instanceof AxiosError) {
-            if (!err.response) {
-                console.log("Network Error")
-            } else {
-                console.log(err.response.status, ":", err.response.data.error)
-            }
-        }
-        else {
-            console.log("Another Error")
-        }
-    }
-}
-
-export const createTask = async (title, desc, status, priority, dueDate) => {
+export const postTask = async (title, desc, status, priority, dueDate) => {
     try {
         const res = await axiosInstance.post("/tasks", {
             title: title,
@@ -49,25 +30,22 @@ export const createTask = async (title, desc, status, priority, dueDate) => {
             priority: priority,
             dueDate: dueDate,
         });
-        return {
-            status: res.status,
-            data: res.data
-        }
+        return res
     } catch (err) {
         if (err instanceof AxiosError) {
             if (!err.response) {
-                console.log("Network Error")
+                throw Error("Network Error")
             } else {
-                console.log(err.response.status, ":", err.response.data.error)
+                throw Error(err.response.data.error)
             }
         }
         else {
-            console.log("Another Error")
+            throw Error("Sorry, Something went wrong!")
         }
     }
 }
 
-export const updateTask = async (id, title, desc, status, priority, dueDate) => {
+export const patchTask = async (id, title, desc, status, priority, dueDate) => {
     try {
         const res = await axiosInstance.patch(`/tasks/${id}`, {
             title: title,
@@ -76,38 +54,53 @@ export const updateTask = async (id, title, desc, status, priority, dueDate) => 
             priority: priority,
             dueDate: dueDate,
         });
-        return {
-            status: res.status,
-            data: res.data
-        }
+        return res
     } catch (err) {
         if (err instanceof AxiosError) {
             if (!err.response) {
-                console.log("Network Error")
+                throw Error("Network Error")
             } else {
-                console.log(err.response.status, ":", err.response.data.error)
+                throw Error(err.response.data.error)
             }
         }
         else {
-            console.log("Another Error")
+            throw Error("Sorry, Something went wrong!")
         }
     }
 }
 
-export const deleteTask = async (id) => {
+export const deleteOneTask = async (id) => {
     try {
         const res = await axiosInstance.delete(`/tasks/${id}`);
-        return res.data
+        return res
     } catch (err) {
         if (err instanceof AxiosError) {
             if (!err.response) {
-                console.log("Network Error")
+                throw Error("Network Error")
             } else {
-                console.log(err.response.status, ":", err.response.data.error)
+                throw Error(err.response.data.error)
             }
         }
         else {
-            console.log("Another Error")
+            throw Error("Sorry, Something went wrong!")
+        }
+    }
+}
+
+export const getTask = async (id) => {
+    try {
+        const res = await axiosInstance.get(`/tasks/${id}`);
+        return res
+    } catch (err) {
+        if (err instanceof AxiosError) {
+            if (!err.response) {
+                throw Error("Network Error")
+            } else {
+                throw Error(err.response.data.error)
+            }
+        }
+        else {
+            throw Error("Sorry, Something went wrong!")
         }
     }
 }
